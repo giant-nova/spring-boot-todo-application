71829ac0dde04cea44c9bdb9ee84a07c0562936d
giant-nova <kunal.datadev.os@gmail.com>
2024-08-03 14:23:23 +0530

    initializing a spring boot todo application

---
+++ src/test/java/com/proj/todo/spring_boot_app/BootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proj.todo.spring_boot_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
